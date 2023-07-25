33ecadd54b8e894daa008844a7931a5be375958b
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-07-25 16:02:56 -0500

    Fixed rendering issue

--- notes.js
+++ notes.js
@@ -26,6 +26,7 @@ router.route('/api/notes').post((req, res) => {
   readAndAppend(body, __dirname + '/db/db.json', (err, data) => {
     err ? console.log(err) : res.sendStatus(201);
   });
+  return data;
 });
 // router.route('/api/notes/:id').delete((req, res) => {
 //   const id = req.params.id;
@@ -48,23 +49,39 @@ router.route('/api/notes').post((req, res) => {
 //   res.sendStatus(204);
 // });
 
-router.route('/api/notes/:id')
-  .delete((req, res) => {
-    const id = req.params.id;
-    readFromFile(__dirname + "/db/db.json", 'utf-8').then((data) => { 
-      console.log(data)
-      const _data = JSON.parse(data)
-      Object.entries(_data).forEach((entry, index) => {
-        console.log(entry[1].id, id)
-        if (entry[1].id == id) {
-          console.log('delete me')
-          _data.splice(index, 1);
-          writeToFile(__dirname + "/db/db.json", _data)
-          return;
-        } 
-      });
+router.route('/api/notes/:id').delete((req, res) => {
+  const id = req.params.id;
+
+  readFromFile(__dirname + '/db/db.json', 'utf-8')
+    .then((data) => {
+      const _data = JSON.parse(data);
+      const indexToDelete = _data.findIndex((entry) => entry.id == id);
+
+      if (indexToDelete === -1) {
+        // If the note with the given UUID is not found, send a 404 Not Found response
+        return res.status(404).json({ message: 'Note not found' });
+      }
+
+      // Delete the note from the array using splice()
+      _data.splice(indexToDelete, 1);
+
+      // Write the updated data back to the file
+      return writeToFile(__dirname + '/db/db.json', _data)
+        .then(() => {
+          // Send a 204 No Content response, indicating the note was successfully deleted
+          res.sendStatus(204);
+        })
+        .catch((error) => {
+          console.error('Error writing data after deletion:', error);
+          res.status(500).json({ message: 'Internal server error' });
+        });
     })
-  })
+    .catch((error) => {
+      console.error('Error deleting note:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
 
 router.route('/').get((req, res) => {
   res.sendFile(__dirname + '/public/index.html');
